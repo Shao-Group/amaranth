e391880e9fee929d9c7529b9a94c988155acac28
Xiaofei (Carl) Zang <xbz5174@psu.edu>
2024-01-18 13:54:46 -0500

    coments

--- src/aster.cc
+++ src/aster.cc
@@ -268,104 +268,151 @@ astron::astron(const aster* _as, const vector<int>& _canons,  const vector<int>&
 	: as(_as), canons(_canons), illegals(_illegal), alternatives(_alts), aster_algo(_algo), dist(-1)
 {
 	classify();
-
-	aster_algo = "heuristic";
 	if 		(aster_algo == "dp") 			dist = dynamic_programming();
-	else if (aster_algo == "dnc")  			dist = divide_and_conquer();
-	else if (aster_algo == "heuristic") 	dist = heuristic();
+	// else if (aster_algo == "dnc")  			dist = divide_and_conquer();
+	// else if (aster_algo == "heuristic") 	dist = heuristic();
+	// else if (aster_algo == "greedy")		dist = greedy();
 	else assert(0);
-
 
 	// if(alternatives.size() == 0) collect_trivial_path();
 	// assert(dist >= 0);
 	// assert(paths.size() >= 1);
 }
 
-int astron::classify()
-{	
-	for (int i: canons)    assert(find(illegals.begin(),  illegals.end(), i)  == illegals.end());
-	for (int i: illegals)  assert(find(canons.begin(),    canons.end(), i)    == canons.end());
-	if(alternatives.size() != 0) return 0;
+// int astron::classify()
+// {	
+// 	for (int i: canons)    assert(find(illegals.begin(),  illegals.end(), i)  == illegals.end());
+// 	for (int i: illegals)  assert(find(canons.begin(),    canons.end(), i)    == canons.end());
+// 	if(alternatives.size() != 0) return 0;
 	
-	int altSize = as->gr.num_vertices() - 2 - canons.size() - illegals.size();
-	assert(altSize >= 0);
-	alternatives.resize(altSize);
-	for (int i = 1; i < as->gr.num_vertices() - 1; i++)
-	{
-		if(find(illegals.begin(),  illegals.end(), i)  != illegals.end()) continue;
-		if(find(canons.begin(),    canons.end(), i)    != canons.end())  continue;
-		alternatives.push_back(i);
-	}	
-	return 0;
-}
+// 	int altSize = as->gr.num_vertices() - 2 - canons.size() - illegals.size();
+// 	assert(altSize >= 0);
+// 	alternatives.resize(altSize);
+// 	for (int i = 1; i < as->gr.num_vertices() - 1; i++)
+// 	{
+// 		if(find(illegals.begin(),  illegals.end(), i)  != illegals.end()) continue;
+// 		if(find(canons.begin(),    canons.end(), i)    != canons.end())  continue;
+// 		alternatives.push_back(i);
+// 	}	
+// 	return 0;
+// }
 
-int astron::divide_and_conquer()
-{
-	int minDist = event_size_penalty(alternatives.size());
+// int astron::divide_and_conquer()
+// {
+// 	int minDist = event_size_penalty(alternatives.size());
 	
-	for(int eventOfConcern: alternatives)
-	{	
-		int exclusiveEvents = 1;
-		int eventPenalty = event_size_penalty(exclusiveEvents);
-		unique_ptr<astron> canonChild; // TODO: one child is enough. Alt child distance can be calculated from canonChild
+// 	for(int eventOfConcern: alternatives)
+// 	{	
+// 		int exclusiveEvents = 1;
+// 		int eventPenalty = event_size_penalty(exclusiveEvents);
+// 		unique_ptr<astron> canonChild; // TODO: one child is enough. Alt child distance can be calculated from canonChild
 
-		assert(canonChild->dist >= 1);
-		// dnc_combine(); // TODO:
-		int newDist = canonChild->dist + eventPenalty;
-		if (minDist > newDist) minDist = newDist;
-	}
+// 		assert(canonChild->dist >= 1);
+// 		// dnc_combine(); // TODO:
+// 		int newDist = canonChild->dist + eventPenalty;
+// 		if (minDist > newDist) minDist = newDist;
+// 	}
 
-	return minDist;
-}
+// 	return minDist;
+// }
 
-int astron::dnc_combine(const vector<path> subpaths, int eventOfConcern)
-{
+// int astron::dnc_combine(const vector<path> subpaths, int eventOfConcern)
+// {
 
-}
+// }
 
+// int astron::greedy()
+// {	
+// 	if(as->gr.num_edges() == 0) return 0;
+// 	for(int i = 1; i < as->gr.num_vertices() - 1; i++) as->balance_vertex(i);
+// 	for(int i = 1; i < as->gr.num_vertices() - 1; i++) as->balance_vertex(i);
 
-/*
-* Search for a new path using a new phasing path
-* s.t. 1. the phasing path is completely contained
-*	   2. the new path has edit distance < `x` from at least 1 of acceptable path
-*	   	2.1 for (x = 1 to 5) if new search found, reset x, otherwise increase x
-*/
-int astron::heuristic()
-{
-	// sort pashing paths by counts
-	vector<vector<int>> ppNodes;
-	vector<int> 		ppCounts;
-	as->hs.sort_nodes(ppNodes, ppCounts);
-	assert(ppNodes.size()  == as->hs.nodes.size());
-	assert(ppCounts.size() == as->hs.nodes.size());
+// 	// int cnt = 0;
+// 	// int n1 = paths.size();
+// 	// while(true)
+// 	// {
+// 	// 	VE v;
+// 	// 	double w = gr.compute_maximum_path_w(v);
+// 	// 	if(w <= min_transcript_coverage) break;
+
+// 	// 	int e = split_merge_path(v, w);
+// 	// 	collect_path(e);
+// 	// 	cnt++;
+// 	// }
+// 	// int n2 = paths.size();
+// 	// if(verbose >= 2) printf("greedy decomposing produces %d / %d paths\n", n2 - n1, n2);
 	
-	vector<edge_descriptor> edges;
-	for(const auto& edgeIndex: as->e2i) edges.push_back(edgeIndex.first);
+// 	greedy_longest_path();
 
-	// heuristics to cover a pp or edge through
-	while (ppNodes.size() != 0 && edges.size() != 0)  
-	{
-		if (heuristic_search(ppNodes)) continue;
-		else heuristic_search(edges);
-	}
+// 	constexpr int maxDistAllow = 5;
+// 	for(int i = 0; i < 5; i++)
+// 	{
+// 		if (greedy_edit_path(i)) continue;
+// 		else break;
+// 	}
+
+// 	return 0;
+// }
+
+// bool astron::greedy_longest_path()
+// {
+// 	const splice_graph& gr = as->gr;
+
+// 	for(int i = 0; i < gr.num_vertices(); i++) {} 
+
+
+// }
+
+
+// /*
+// * Search for a new path using a new phasing path
+// * s.t. 1. the phasing path is completely contained
+// *	   2. the new path has edit distance < `x` from at least 1 of acceptable path
+// *	   	2.1 for (x = 1 to 5) if new search found, reset x, otherwise increase x
+// */
+// int astron::heuristic()
+// {
+// 	// sort pashing paths by counts
+// 	vector<vector<int>> ppNodes;
+// 	vector<int> 		ppCounts;
+// 	as->hs.sort_nodes(ppNodes, ppCounts);
+// 	assert(ppNodes.size()  == as->hs.nodes.size());
+// 	assert(ppCounts.size() == as->hs.nodes.size());
+	
+// 	vector<edge_descriptor> edges;
+// 	for(const auto& edgeIndex: as->e2i) edges.push_back(edgeIndex.first);
+
+// 	// heuristics to cover a pp or edge through
+// 	while (ppNodes.size() != 0 && edges.size() != 0)  
+// 	{
+// 		if (heuristic_search(ppNodes)) continue;
+// 		else if (heuristic_search(edges)) continue;
+// 		break;
+// 	}
 	
 
-	return dist;
-}
+// 	return dist;
+// }
 
-/* try to cover a pp through heuristics
-*/
-bool astron::heuristic_search(vector<vector<int>>& ppNodes)
-{
+// /* try to cover a pp through heuristics
+// */
+// bool astron::heuristic_search(vector<vector<int>>& ppNodes, int maxDist)
+// {
+// 	for(auto it = ppNodes.begin(); it != ppNodes.end(); it ++)
+// 	{
+// 		const vector<int>& pp = *it;
+// 		int pathIndex = closest_path(pp, 1);
+// 		if (pathIndex < 0) continue;
+// 		assert(pathIndex >= 0 && pathIndex < paths.size());
+// 	}
+// }
 
-}
+// /* try to cover an edge through heuristics
+// */
+// bool astron::heuristic_search(vector<edge_descriptor>& edges, int maxDist)
+// {
 
-/* try to cover an edge through heuristics
-*/
-bool astron::heuristic_search(vector<edge_descriptor>& edges)
-{
-
-}
+// }
 
 int astron::dynamic_programming()
 {
